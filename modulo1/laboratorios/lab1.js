abf1217c0f48c0d589c6c546489df97770143533
maria <mpramos78915400@gmail.com>
2024-04-01 20:38:57 -0400

    laboratorio del modulo 1

---
+++ modulo1/laboratorios/lab1.js
@@ -0,0 +1,25 @@
+// Maxwell Wright	(0191) 719 6495	Curabitur.egestas.nunc@nonummyac.co.uk
+// Raja Villarreal	0866 398 2895	posuere.vulputate@sed.com
+// Helen Richards	0800 1111	libero@convallis.edu
+
+//! PRIMER LABORATORIO
+
+let nombreContacto1="Maxwell Wright" //string
+let nombreContacto2="Raja Villarrea" //string
+let nombreContacto3="Helen Richards" //string
+
+let correoElectronicoContacto1="Curabitur.egestas.nunc@nonummyac.co.uk" //string
+let correoElectronicoContacto2="posuere.vulputate@sed.com" //string
+let correoElectronicoContacto3="libero@convallis.edu" //string
+
+let telefonoContacto1="(0191) 719 6495" //string
+let telefonoContacto2="0866 398 2895" //string
+let telefonoContacto3="0800 1111" //string
+
+
+//? Primera forma 
+// console.log(nombreContacto1 + '/' + telefonoContacto1 + '/' + correoElectronicoContacto1);
+// console.log(nombreContacto3 + '/' + telefonoContacto3 + '/' + correoElectronicoContacto3);
+//? Segunda forma 
+console.log(`${nombreContacto1}/${telefonoContacto1}/${correoElectronicoContacto1}`);
+console.log(`${nombreContacto3}/${telefonoContacto3}/${correoElectronicoContacto3}`);
