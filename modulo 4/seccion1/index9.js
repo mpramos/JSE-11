32c8cf3d7b5ef9befe92ec420ac81a4ef7493d8f
maria <mpramos78915400@gmail.com>
2024-04-04 22:07:58 -0400

    avance del if con la familia de los simpsons

---
+++ modulo 4/seccion1/index9.js
@@ -0,0 +1,25 @@
+let deseo = ''
+let integrante = 'ned'
+if (integrante === 'magui') {
+    console.log('🍭');
+} else if(integrante === 'bart') {
+    console.log('🤑💲💰💹');
+} else if(integrante === 'lisa'){
+    console.log('🌐🗺 🏳');
+} else if(integrante === 'homero'){
+    console.log('🥪');
+} else {
+    console.log('el deseo lo está piddiendo el vecino de Homero ')
+}
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
