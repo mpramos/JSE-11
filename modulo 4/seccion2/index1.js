b171e4613cdae85dad4139d6deec66737a269655
maria <mpramos78915400@gmail.com>
2024-04-11 20:44:50 -0400

    ejercicios con el for, primer reto

---
+++ modulo 4/seccion2/index1.js
@@ -0,0 +1,34 @@
+//!sintaxis del bucle
+for (initializacion; condition; increment/decrement) {
+// code     
+}
+// 0 1 2 3 4 5
+for (let i = 0; i <6 ; i++) { // i = 0
+    console.log(i);
+}
+// 5 4 3 2 1 0
+for (let i = 5; i>=0 ; i--) { // i = 4
+    console.log(i);
+}
+
+for (let i = 0; i <=5; i++) {
+    console.log(`${i} * ${i} = ${i*i}`);
+}
+/*
+0 * 0 =0 
+1 * 1 =1
+2 * 2 =4
+3 * 3 =9
+4 * 4 =16
+5 * 5 =25
+*/
+const countries = ['Peru','Bolivia','Argentina','Chile','Ecuador']
+// ['PERU','BOLIVIA','ARGENTINA','CHILE','ECUADOR']
+let nombre = 'juana'
+console.log(nombre.toUpperCase());
+
+
+
+
+
+
